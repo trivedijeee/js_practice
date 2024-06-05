9112479fdbd069737526dd12617e118121d28851
trivedijeee <trivedijeee@gmail.com>
2024-06-06 04:52:33 +0530

    first letter upper case or rest leter in lowwer by usong prompt / slice / toupper/ tolower / concatentation / alert and adding dog age calculaor

--- scrpt.js
+++ scrpt.js
@@ -6,4 +6,6 @@ RestLetterSlice = RestLetterSlice.toLowerCase();
 alert("hi " + FirstLetterSlice + RestLetterSlice + ", " + "Thanks for entering your name.");
 
 
-
+var dogAge = prompt("enter your dog age");
+var HumanAge =  ((dogAge - 2) * 4 +21);
+alert(HumanAge);
