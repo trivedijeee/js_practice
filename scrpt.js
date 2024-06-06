a72f503c7a522d6107a44fba5c2cec921fad4c52
trivedijeee <trivedijeee@gmail.com>
2024-06-06 14:07:26 +0530

    arthmetic and the modulo operator

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,4 @@
+// name prompt + slice + concatation + alert
 var MyName = prompt("your name");
 var FirstLetterSlice = MyName.slice(0,1);
 var RestLetterSlice = MyName.slice(1, (MyName.length))
@@ -5,7 +6,17 @@ FirstLetterSlice = FirstLetterSlice.toUpperCase();
 RestLetterSlice = RestLetterSlice.toLowerCase();
 alert("hi " + FirstLetterSlice + RestLetterSlice + ", " + "Thanks for entering your name.");
 
+// Arthmetic and the modulo operator
 
+var addition = alert(2+3);
+var Subtract = alert(2-3);
+var multiply = alert(2*3);
+var division = alert(18/3);
+var _Modulo = alert(2%3);
+
+
+// calculate the age of dog as compare to human 
 var dogAge = prompt("enter your dog age");
 var HumanAge =  ((dogAge - 2) * 4 +21);
 alert(HumanAge);
+
