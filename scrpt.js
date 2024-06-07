b4b6dbd110508d0dd40d26671939ea75617c77ff
trivedijeee <trivedijeee@gmail.com>
2024-06-07 15:54:22 +0530

    function

--- scrpt.js
+++ scrpt.js
@@ -1,3 +1,4 @@
+
 // name prompt + slice + concatation + alert
 var MyName = prompt("your name");
 var FirstLetterSlice = MyName.slice(0,1);
@@ -20,3 +21,39 @@ var dogAge = prompt("enter your dog age");
 var HumanAge =  ((dogAge - 2) * 4 +21);
 alert(HumanAge);
 
+// Function - creating and calling
+function getMilk() {
+    alert("leave house");
+    alert("take one step");
+    alert("take right");
+    alert("take one step");
+    alert("back to home");
+}
+getMilk();
+
+//life in weeks exercise
+function lifeInWeeks(age) {
+    var yearRemeaning = 90-age;
+    var days = yearRemeaning*365;
+    var weeks = yearRemeaning*52;
+    var months = yearRemeaning*12;
+
+    console.log("You have " + days +" days," + weeks + " weeks, and " +months+" months left.");
+    alert("You have " + days +" days, " + weeks + " weeks, and " +months+" months left.");
+}
+lifeInWeeks(51);
+
+// get milk from shop..
+function shopMilk(money , costPerBottel){
+    console.log("buy " + calcBottels(money,costPerBottel)+ " bottels of milk...")
+    return calcchange(money , costPerBottel);
+}
+function calcBottels (startingMoney ,costPerBottel){
+    var numberOfBottels = Math.floor(startingMoney/costPerBottel);
+    return numberOfBottels;
+}
+function calcchange(startingMoney , costPerBottel){
+    var change = startingMoney % costPerBottel;
+    return change;
+}
+console.log("Hello master, here is your "+ shopMilk(5 ,1.3 ) + " change..");
