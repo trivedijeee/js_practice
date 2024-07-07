a853f8a12dbaf6dee337bccb1243d543f70f6f2c
trivedijeee <trivedijeee@gmail.com>
2024-07-07 16:52:45 +0530

    cal

--- scrpt.js
+++ scrpt.js
@@ -317,3 +317,24 @@ else {
 //     rollDice();
 //   };
   
+
+
+// calculator using js function also using higher order function (function that can take other function as input)
+function add(num1, num2){
+    return num1 + num2;
+}
+function sub(num1, num2){
+    return num1 - num2;
+}
+function mul(num1, num2){
+    return num1 * num2;
+}
+function div(num1, num2){
+    return num1 / num2;
+}
+function rem(num1, num2){
+    return num1 % num2;
+}
+function calculator(num1, num2, operator){
+    return operator(num1, num2);
+}
