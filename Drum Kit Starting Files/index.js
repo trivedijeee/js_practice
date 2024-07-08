15005dc89db1629f9fec23a914f8cb4aa730bd92
trivedijeee <trivedijeee@gmail.com>
2024-07-08 14:16:12 +0530

    nothing

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,9 +1,9 @@
-// // single element event listner
+// single element event listner
 
-// // document.querySelector("button").addEventListener("click", handleClick);
-// // function handleClick(){
-// //     alert("i got clicked!");
-// // }
+// document.querySelector("button").addEventListener("click", handleClick);
+// function handleClick(){
+//     alert("i got clicked!");
+// }
 
 
 // // another way for single element event listner
@@ -29,31 +29,39 @@
 // }
 
 // Writing the seprate code for all seprate button
-//for W
+// for W
 document.querySelectorAll(".drum")[0].addEventListener("click", function(){
-    alert(" W is getting clicked!")
+    var audio = new Audio('sounds/crash.mp3')
+    audio.play();
+    // alert(" W is getting clicked!")
 });
 //for a
 document.querySelectorAll(".drum")[1].addEventListener("click", function(){
-    alert(" A is getting clicked!")
+    var audio = new Audio('sounds/kick-bass.mp3')
+    audio.play();
 });
 // for s
 document.querySelectorAll(".drum")[2].addEventListener("click", function(){
-    alert(" S is getting clicked!")
+    var audio = new Audio('sounds/snare.mp3')
+    audio.play();
 });
 //For d
 document.querySelectorAll(".drum")[3].addEventListener("click", function(){
-    alert(" D is getting clicked!")
+    var audio = new Audio('sounds/tom-1.mp3')
+    audio.play();
 });
 //for j
 document.querySelectorAll(".drum")[4].addEventListener("click", function(){
-    alert(" J is getting clicked!")
+    var audio = new Audio('sounds/tom-2.mp3')
+    audio.play();
 });
 //for k
 document.querySelectorAll(".drum")[5].addEventListener("click", function(){
-    alert(" K is getting clicked!")
+    var audio = new Audio('sounds/tom-3.mp3')
+    audio.play();
 });
 //for l
 document.querySelectorAll(".drum")[6].addEventListener("click", function(){
-    alert(" L is getting clicked!")
+    var audio = new Audio('sounds/tom-4.mp3')
+    audio.play();
 });
